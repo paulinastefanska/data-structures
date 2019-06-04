fab235c0937e59e0707ad397efc51bdffa644aea
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-04 09:22:45 +0200

    Add simple html, style and start js

---
+++ js/script.js
@@ -0,0 +1,43 @@
+'use strict';
+(function(){ 
+
+var data = [
+	{
+		id: 'box1',
+		title: 'First box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'special-header', 'important']
+	},
+	{
+		id: 'box2',
+		title: 'Second box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['special-header', 'important']
+	},
+	{
+		id: 'box3',
+		title: 'Third box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'important']
+	},
+	{
+		id: 'box4',
+		title: 'Fourth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted']
+	},
+	{
+		id: 'box5',
+		title: 'Fifth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: []
+	},
+];
+
+var boxes = document.querySelectorAll('.box');
+  
+  for( var i = 0; i < boxes.length; i++ ){
+		var boxHeader = boxes[i].querySelectorAll('.title');
+      boxHeader.innerHTML = 'Box id:' + boxes[i].id;
+    
+}();
